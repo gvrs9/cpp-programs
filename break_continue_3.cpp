3d3e8eb24f32ff6dcf332ec3bd4a87eebdc3aa3f
GVRS sai Ganesh <63167616+gvrs9@users.noreply.github.com>
2020-10-25 22:46:11 +0530

    break_continue_3.cpp added

---
+++ break_continue_3.cpp
@@ -0,0 +1,18 @@
+/*Write a program to print all odd numbers till n.*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 0; i <=n; i++) {
+        if(i%2==0){
+            continue;
+        }
+        cout<<i<<endl;
+    }
+    return 0;
+}
